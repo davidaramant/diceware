574950255fc3f31dad61ee5751a051ebf62fb4e5
David Aramant <davidaramant@gmail.com>
2021-10-19 09:29:38 -0400

    Support for generating a bunch of phrases

--- Program.cs
+++ Program.cs
@@ -4,15 +4,19 @@ using System.IO;
 using System.Linq;
 using SecurityDriven.Core;
 
-int numberOfWords = (args.Length > 0 && int.TryParse(args[0], out var parsed)) ? parsed : 6;
+int numberOfWords = TryReadIntArg(args, 0) ?? 6;
+int numberOfPhrases = TryReadIntArg(args, 1) ?? 1;
 var wordList = LoadWordList();
 
-var passphrase =
-    string.Join(' ',
-        Enumerable.Range(0, numberOfWords)
-            .Select(_ => wordList[CryptoRandom.Shared.Next(wordList.Count)]));
+foreach (var phraseNum in Enumerable.Range(1, numberOfPhrases))
+{
+    var passphrase =
+        string.Join(' ',
+            Enumerable.Range(0, numberOfWords)
+                .Select(_ => wordList[CryptoRandom.Shared.Next(wordList.Count)]));
 
-Console.Out.WriteLine(passphrase);
+    Console.Out.WriteLine(passphrase);
+}
 
 static IReadOnlyList<string> LoadWordList() =>
     File
@@ -20,3 +24,9 @@ static IReadOnlyList<string> LoadWordList() =>
         .TakeWhile(line => !string.IsNullOrWhiteSpace(line))
         .Select(line => line.Split(' ')[1])
         .ToList();
+
+static int? TryReadIntArg(string[] args, int index) =>
+    args.Length > index &&
+    int.TryParse(args[index], out var parsed)
+    ? parsed
+    : null;
